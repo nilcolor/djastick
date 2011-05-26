f2385ba9389df9a1582cf2e8921da0df51e6040a
Alexey Blinov <nilcolor@gmail.com>
2011-05-26 23:47:53 +0400

    Added method (PUT/DELETE) override middleware

--- src/djastick/core/middleware.py
+++ src/djastick/core/middleware.py
@@ -1,5 +1,26 @@
 # encoding: utf-8
 
-class RestMiddleware(object):
-    pass
+import re
+import itertools
+
+class HttpMethodsOverride(object):
+    """
+    Method override based on <.. name="_method" value="put" /> elements.
+    Used to deal with current browser limitation.
+    Don't necessary if you can use true PUT/DELETE methods.
+    """
+    SUPPORTED_TRANSFORMS = ['POST','PUT', 'DELETE']
+    MIDDLEWARE_KEY = '_method'
+    
+    def process_request(self, request):
+        if request.POST and request.POST.has_key(self.MIDDLEWARE_KEY):
+            if request.POST[self.MIDDLEWARE_KEY].upper() in self.SUPPORTED_TRANSFORMS:
+                request.method = request.POST[self.MIDDLEWARE_KEY].upper()
+        else:
+            request.method = 'GET'
+        
+        print request.method
+        
+        return None
+
 
--- src/example_com/settings.py
+++ src/example_com/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'djastick.core.middleware.HttpMethodsOverride',
     'djastick.core.middleware.RestMiddleware',
 )
 
