f285caae72471e8c6e49aa95df6a1b8277c39d75
Alexey Blinov <nilcolor@gmail.com>
2011-05-27 12:51:49 +0400

    Duplicate Resource removed

--- src/example_com/shop/resources.py
+++ src/example_com/shop/resources.py
@@ -2,18 +2,7 @@
 from django.http import HttpResponse
 from djastick.core.resources import Resource
 
-class GoodsListResource(Resource):
-    """
-    List of goods.
-    """
-    def get(self, request):
-        from djastick.core import STATUS
-        
-        # return HttpResponse('Hallo djastick!!!')
-        return STATUS.NOT_IMPLEMENTED
-
-
-class GoodsListResource(Resource):
+class GoodsListResource(object):
     """
     List of goods.
     """
